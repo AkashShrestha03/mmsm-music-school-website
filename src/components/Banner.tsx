b7535bccceff04ae4fdafb2f444ef3cf64f92bba
SamriddhiRoy <samriddhi7564@gmail.com>
2025-08-25 22:25:22 +0530

    Fixed banner positioning - changed from margin to padding to properly clear navbar

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -4,17 +4,15 @@ import Image from 'next/image';
 
 const Banner = () => {
   return (
-    <section className="relative w-full bg-black pt-16 sm:pt-18 md:pt-20 lg:pt-24 xl:pt-28">
-      {/* Background Image */}
-      <div className="relative w-full h-[300px] sm:h-[350px] md:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[650px] overflow-hidden">
+    <section className="relative w-full pt-20">
+      <div className="relative w-full h-[250px] sm:h-[350px] md:h-[450px] lg:h-[550px] xl:h-[650px] overflow-hidden">
         <Image
           src="/banner-1.png"
           alt="MOUJ MAALIK Banner"
           fill
           priority
           sizes="100vw"
-          className="object-contain sm:object-cover object-center"
-          style={{ width: '100%', height: '100%' }}
+          className="object-cover object-center"
         />
       </div>
     </section>
@@ -22,5 +20,6 @@ const Banner = () => {
 };
 
 export default Banner;
+
 
 
